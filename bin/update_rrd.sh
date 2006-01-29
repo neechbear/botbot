e412104dfd25f9b83004424d67062574856ec223
Nicola Worthington <nicolaw@tfb.net>
2006-01-29 20:58:35 +0000

    Only run commands if the chdir works
    git-svn-id: file:///var/tmp/data/svnroot/botbot@5 610ec7d1-1c12-4784-ad90-13d12571cf29

--- bin/update_rrd.sh
+++ bin/update_rrd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 umask 0002
-cd /home/system/colloquy/botbot
-(zcat logs/botbot.log.1.gz;cat logs/botbot.log) | ./rrd.pl
+cd /home/system/colloquy/botbot && \
+	(zcat logs/botbot.log.1.gz;cat logs/botbot.log) | ./rrd.pl
 
